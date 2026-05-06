eda901c94355f53efaa720f8d42dfe6e85ee4ace
MarianaNietoSilva <mariananietodaw@gmail.com>
2026-05-06 08:30:58 +0200

    GUARDADO 1
    Se crean las ventanas

---
+++ pokemon/src/module-info.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module pokemon {
+	requires org.eclipse.jface.text;
+	requires jakarta.annotation-api;
+	requires org.eclipse.jface;
+	requires org.eclipse.core.runtime;
+	requires org.eclipse.osgi;
+	requires com.ibm.icu;
+	requires org.eclipse.equinox.registry;
+	requires org.eclipse.core.commands;
+	requires org.eclipse.ui.forms;
+	requires org.eclipse.ui.workbench;
+	requires org.eclipse.text;
+	requires org.eclipse.e4.ui.di;
+	requires org.eclipse.swt.win32.win32.x86_64;
+	requires org.eclipse.equinox.common;
+}
